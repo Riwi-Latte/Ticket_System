fd1a1803477fc2ef09ac8cf297c56be3fe60fa40
isabellaPulgarin <isabellapul7@gmail.com>
2025-10-07 17:55:11 -0500

    fix: organize the connection, call it in the query at once

--- src/dao/categoryDao.java
+++ src/dao/categoryDao.java
@@ -6,23 +6,31 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import config.DbConfig;
 import domain.categoryDomain;
 
 public class categoryDao {
     
-    private Connection connection; 
 
-    public categoryDao(Connection connection) {
-        this.connection = connection;
-    }
+    public categoryDao() {    
+    };
 
     public void createCategoryDao(String name) {
         
+            System.out.println("Creando categoria: " + name);
         String sql = "INSERT INTO categories (category_name) VALUES (?)";
-        try (Connection conn = connection;
-             PreparedStatement ps = conn.prepareStatement(sql)) {
+        try (Connection conn = DbConfig.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS)) {
             ps.setString(1, name);
             ps.executeUpdate();
+            try (ResultSet generatedKeys = ps.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    int newId = generatedKeys.getInt(1);
+                    System.out.println("Categoria creada con ID: " + newId);
+                } else {
+                    System.out.println("No se pudo obtener el ID de la nueva categoria.");
+                }
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -31,7 +39,7 @@ public class categoryDao {
     public void deleteCategoryDao(int id) {
         
         String sql = "DELETE FROM categories WHERE category_id = ?";
-        try (Connection conn = connection;
+        try (Connection conn = DbConfig.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setInt(1, id);
             ps.executeUpdate();
@@ -40,15 +48,15 @@ public class categoryDao {
 
         }
 
-    }
+    };
 
 
     public ArrayList<categoryDomain> listCategoriesDao() {
 
         ArrayList<categoryDomain> lista = new ArrayList<>();
         
-        String sql = "SELECT * FROM categories";
-        try (Connection conn = connection;
+        String sql = "SELECT * FROM categories;";
+        try (Connection conn = DbConfig.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
             ResultSet res = ps.executeQuery();
 
